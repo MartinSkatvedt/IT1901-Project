ed2bc154f7da8ff2234e66682c9cd922d2602690
Martinskatvedt <martinskatvedt@gmail.com>
2021-09-14 12:16:03 +0200

    added date to the event class

--- calendar/core/src/main/java/calendar/core/Event.java
+++ calendar/core/src/main/java/calendar/core/Event.java
@@ -1,21 +1,28 @@
 package calendar.core;
 
+import java.util.Date;
+
 public class Event {
     private String header;
     private String description;
+    private Date date;
 
-    public Event(String headerArg, String descArg) {
+    public Event(String headerArg, String descArg, Date dateArg) {
         this.header = headerArg;
         this.description = descArg;
+        this.date = dateArg;
     }
 
     public void setHeader(String newHeader) {
         this.header = newHeader;
     }
 
-
     public void setDescription(String newDesc) {
         this.description = newDesc;
+    }
+
+    public void setDate(Date newDate) {
+        this.date = newDate;
     }
 
     public String getHeader() {
@@ -24,6 +31,10 @@ public class Event {
 
     public String getDescription() {
         return this.description;
+    }
+
+    public Date getDate() {
+        return this.date;
     }
 }
 
