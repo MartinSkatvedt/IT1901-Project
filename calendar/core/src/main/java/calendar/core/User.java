323291a1e01a651195ffab86ce07ee1705d55115
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2021-09-17 15:24:35 +0200

    Laget User-klasse med noen metoder

---
+++ calendar/core/src/main/java/calendar/core/User.java
@@ -0,0 +1,45 @@
+package calendar.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String username;
+    //private Calendar calendar;
+    private List<User> friends = new ArrayList<>();
+    
+    public User(final String username) {
+        //Sjekke om det er gyldig brukernavn?
+        this.username = username;
+        //this.calendar = new Calendar();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+   /* public Calendar getCalendar() {
+        return calendar;
+    }*/
+
+    public void addFriend(User newFriend) {
+        if (friends.contains(newFriend)) {
+            throw new IllegalArgumentException("This user is already a friend");
+        }
+        friends.add(newFriend);
+    }
+
+  /*  public Calendar getFriendsCalendar(User friend) {
+        if (!friends.contains(friend)) {
+            throw new IllegalArgumentException("This user is not a friend");
+        }
+        return friend.getCalendar();
+    }
+
+    public void resetCalendar() {
+        this.calendar = new Calendar();
+    }*/
+ 
+}
+
