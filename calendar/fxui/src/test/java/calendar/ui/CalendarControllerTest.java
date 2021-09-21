e954d67666753264b4303f7b56eee3012e6cbf2d
Martin Skatvedt <martinskatvedt@gmail.com>
2021-09-21 23:22:27 +0200

    added test to make surefire work

---
+++ calendar/fxui/src/test/java/calendar/ui/CalendarControllerTest.java
@@ -0,0 +1,12 @@
+package calendar.ui;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalendarControllerTest {
+
+    @Test
+    public void testTemplate() {
+        assertEquals("abc", "abc");
+    }
+}
