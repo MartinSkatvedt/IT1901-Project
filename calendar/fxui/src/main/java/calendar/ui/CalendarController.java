c8128159f23b3b5ceb1bade74ea5d7ca7d630db2
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2021-10-05 12:34:30 +0200

    slettet det geir la inn

--- calendar/fxui/src/main/java/calendar/ui/CalendarController.java
+++ calendar/fxui/src/main/java/calendar/ui/CalendarController.java
@@ -1,27 +1,5 @@
 package calendar.ui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.event.ActionEvent;
-
-import java.io.IOException;
-
-
-
 public class CalendarController {
 
-    @FXML private GridPane gp;
-
-    @FXML private Button logOutButton;
-
-
-    public void logOut(ActionEvent event) throws IOException {
-        CalendarApp calendarApp = new CalendarApp();
-        calendarApp.changeScene("Login.fxml");
-    
-        
-    }
-
-   
 }
