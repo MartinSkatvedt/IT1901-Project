ce177686b45f2f02c3e9e5b63db3882776140bb5
Eiril Solveig Ugulen <eirilsu@ntnu.no>
2021-10-05 11:00:34 +0200

    created EventController

---
+++ calendar/fxui/src/main/java/calendar/ui/EventController.java
@@ -0,0 +1,17 @@
+package calendar.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class EventController {
+
+    @FXML private TextField title, date, time; 
+    @FXML private TextArea description; 
+
+    @FXML
+    void initialize() {
+
+    }
+    
+}
