61d02c37200e9dd335cd73c45882883304e130e5
egilhage <egilhage@stud.ntnu.no>
2021-10-05 11:04:20 +0200

    Linked loginController to calendarController

--- calendar/fxui/src/main/java/calendar/ui/CalendarApp.java
+++ calendar/fxui/src/main/java/calendar/ui/CalendarApp.java
@@ -1,19 +1,38 @@
 package calendar.ui;
 
+
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+//import jdk.javadoc.internal.doclets.formats.html.resources.standard;
 
 public class CalendarApp extends Application {
 
+    private Stage stage;
+    
+
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
-        stage.setScene(new Scene(parent));
-        stage.show();
+    public void start(Stage firstStage) throws Exception {
+        this.stage = firstStage;
+        firstStage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        firstStage.setTitle("CoolCalendar");
+        firstStage.setScene(new Scene(root, 350, 400));
+        firstStage.show();
     }
+    
+     public void changeScene(String fxml) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = new Scene(parent, 600, 500);
+        this.stage.setScene(scene);
+
+    } 
+
+
 
     public static void main(String[] args) {
         launch(CalendarApp.class, args);
--- calendar/fxui/src/main/java/calendar/ui/LoginController.java
+++ calendar/fxui/src/main/java/calendar/ui/LoginController.java
@@ -1,11 +1,50 @@
 package calendar.ui;
 
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 
 public class LoginController {
-    
-    @FXML
-    void initialize() {
+
+    public LoginController(){
 
     }
+
+    @FXML private Label loginReply;
+
+    @FXML private TextField username;
+
+    @FXML private PasswordField password;
+
+    @FXML private Button loginButton;
+
+    
+    public void  login(ActionEvent event) throws IOException {
+        if (username.getText().toString().isEmpty() || password.getText().isEmpty()) {
+            loginReply.setText("Please input username and password");
+        }
+
+        else {
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            loginReply.setText("Welcome");
+            Parent root = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            //userAccess();
+        }
+    }
+
+   
 }
