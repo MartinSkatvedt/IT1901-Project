dd49189f94be4cb69ba554cf2b49769f975f6e2a
Eiril Solveig Ugulen <eirilsu@ntnu.no>
2021-10-12 11:47:20 +0200

    created EventController and changed date to DatePicker in Event.fxml

--- calendar/fxui/src/main/java/calendar/ui/EventController.java
+++ calendar/fxui/src/main/java/calendar/ui/EventController.java
@@ -1,17 +1,34 @@
 package calendar.ui;
 
+import java.time.LocalDate;
+
+import calendar.core.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class EventController {
 
-    @FXML private TextField title, date, time; 
+    @FXML private TextField title, time; 
+    @FXML private DatePicker date;  
     @FXML private TextArea description; 
+    @FXML private Button addEvent; 
 
     @FXML
     void initialize() {
+        
+    }   
+
+    @FXML
+    private void onAddEvent() {
+        String title = this.title.getText(); 
+        LocalDate date = this.date.getValue(); 
+       // String time = this.time.getText(); 
+        String description = this.description.getText(); 
+        Event event = new Event(title, description, date); 
+
 
     }
-    
 }
