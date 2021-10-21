87cc1435f3d2a455791251fe87e38bb9a64af515
Martin Skatvedt <martinskatvedt@gmail.com>
2021-10-21 13:15:55 +0200

    now tests json

--- calendar/core/src/test/java/calendar/json/UserPersistenceTest.java
+++ calendar/core/src/test/java/calendar/json/UserPersistenceTest.java
@@ -1,15 +1,11 @@
 package calendar.json;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import calendar.core.Event;
@@ -23,16 +19,22 @@ public class UserPersistenceTest {
     @DisplayName("Testing serializers and deserializers")
     public void persistenceTest() {
         
-        userPersistence.setSaveFile("testJSON");
+        userPersistence.setSaveFile("testJSON.json");
         User user = new User("testUserName");
         Event event1 = new Event("event1", "eventDesc1", LocalDate.of(2021, 10, 10), "12:00");
         user.getCalendar().addEvent(event1);
+        Event event2 = new Event("event2", "eventDesc2", LocalDate.of(2021, 1, 1), "12:00");
+        user.getCalendar().addEvent(event2);
 
         try {
             userPersistence.saveUser(user);
+            User loadedUser = userPersistence.loadUser();
+            assertEquals(user.getUsername(), loadedUser.getUsername());
+            assertEquals(user.getCalendar().getEvents().size(), loadedUser.getCalendar().getEvents().size());
+            assertEquals(user.getCalendar().getEvents().get(0).getHeader(), loadedUser.getCalendar().getEvents().get(0).getHeader());
+            assertEquals(user.getCalendar().getEvents().get(1).getHeader(), loadedUser.getCalendar().getEvents().get(1).getHeader());
         } catch (Exception e) {
             fail(e.getMessage());
         }
-    }
-    
+    }   
 }
