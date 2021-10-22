3ccb61d13344a5c755c27ef49f6e3c1389950339
egilhage <egilhage@stud.ntnu.no>
2021-10-22 18:10:57 +0200

    Changes TestFX

--- calendar/fxui/src/test/java/calendar/ui/LoginTest.java
+++ calendar/fxui/src/test/java/calendar/ui/LoginTest.java
@@ -4,36 +4,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-//import javafx.scene.control.ListView;
-//import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-//import java.util.List;
-//import java.util.stream.Stream;
 
-//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.testfx.api.FxToolkit;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.MethodSource;
-//import org.junit.jupiter.params.provider.EmptySource;
 import org.testfx.framework.junit5.ApplicationTest;
-//import org.testfx.matcher.control.LabeledMatchers;
-
-import calendar.core.User;
 
 
 public class LoginTest extends ApplicationTest {
 
     private LoginController controller;
     private Parent root;
-    private User currentUser;
 
     @Override
     public void start(Stage stage) throws IOException {
