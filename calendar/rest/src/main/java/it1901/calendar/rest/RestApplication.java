79120bf3d6246930ec91909947c958e5ad8390b3
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2021-10-27 12:20:05 +0200

    set up rest module

---
+++ calendar/rest/src/main/java/it1901/calendar/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package it1901.calendar.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
