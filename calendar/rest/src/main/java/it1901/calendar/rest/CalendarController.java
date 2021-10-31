e04c3f6ea6f3118330caff2519c17095f64fc6af
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2021-10-31 13:23:22 +0100

    started with controller

--- calendar/rest/src/main/java/it1901/calendar/rest/CalendarController.java
+++ calendar/rest/src/main/java/it1901/calendar/rest/CalendarController.java
@@ -1,3 +1,27 @@
+package it1901.calendar.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import calendar.core.Calendar;
+import calendar.core.User;
+
+/**
+ * Used for requests for a users calendar PUT for adding a event POST for
+ * editing event DELETE for deleting a event
+ */
+
+@RestController
+@RequestMapping("/calendar")
 public class CalendarController {
-    
+
+    private Calendar calendar;
+
+    @PutMapping(path = "/{header}", consumes = "application/json", produces = "application/json")
+    public boolean putEvent() {
+        return true;
+    }
+
 }
