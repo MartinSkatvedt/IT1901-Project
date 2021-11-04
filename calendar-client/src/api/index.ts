b9c6ae99ebe08ab6a4f02935e399b18bd26e471f
Martinskatvedt <martinskatvedt@gmail.com>
2021-11-04 15:13:27 +0100

    added inital methods

--- calendar-client/src/api/index.ts
+++ calendar-client/src/api/index.ts
@@ -1,15 +1,71 @@
 import axios from "axios";
-import { User } from "../types/user";
-export const DEFAULT_URL = "localhost:8000/api/";
-export const USER_URL = DEFAULT_URL + "user";
+import { User, EventType, CalendarType } from "../types/user";
+import {createCalendarResponseType} from "../types/api";
 
-export const getUser = async (): Promise<User | undefined> => {
+export const DEFAULT_URL = "localhost:8080/api/";
+export const USER_URI = (username: string) => `${DEFAULT_URL}/user/${username}`;
+export const CALENDAR_URI = (username: string) => `${DEFAULT_URL}/calendar/${username}`;
+export const CALENDAR_ID_URI = (username: string, id: number) => `${CALENDAR_URI(username)}/${id}`;
+
+
+export const getUser = async (username: string): Promise<User | undefined> => {
 	const response = await axios
-		.get<User>(USER_URL)
+		.get<User>(USER_URI(username))
 		.catch(() => Promise.resolve(undefined));
 
 	if (response && response.status === 200) {
 		return response.data as User;
 	}
 	else return undefined;
+};
+
+export const getEvents = async (username: string): Promise<CalendarType | undefined> => {
+	const response = await axios
+		.get<CalendarType>(CALENDAR_URI(username))
+		.catch(() => Promise.resolve(undefined));
+
+	if (response && response.status === 200) {
+		return response.data as CalendarType;
+	} else return undefined;
+};
+
+export const getEventByID = async (username: string, id: number): Promise<EventType | undefined> => {
+	const response = await axios
+		.get<EventType>(CALENDAR_ID_URI(username, id))
+		.catch(() => Promise.resolve(undefined));
+
+	if (response && response.status === 200) {
+		return response.data as EventType;
+	} else return undefined;
+};
+
+export const createEvent = async (username: string, event: EventType): Promise<createCalendarResponseType | undefined> => {
+	const response = await axios
+		.post<createCalendarResponseType>(CALENDAR_URI(username), event)
+		.catch(() => Promise.resolve(undefined));
+
+	if (response && response.status === 200) {
+		return response.data as createCalendarResponseType;
+	} else return undefined;
+};
+
+export const updateEvent = async (username: string, id: number): Promise<createCalendarResponseType | undefined> => {
+	const response = await axios
+		.put<createCalendarResponseType>(CALENDAR_ID_URI(username, id))
+		.catch(() => Promise.resolve(undefined));
+
+	if (response && response.status === 200) {
+		return response.data as createCalendarResponseType;
+	} else return undefined;
+};
+
+
+export const deleteEvent = async (username: string, id: number): Promise<createCalendarResponseType | undefined> => {
+	const response = await axios
+		.delete<createCalendarResponseType>(CALENDAR_ID_URI(username, id))
+		.catch(() => Promise.resolve(undefined));
+
+	if (response && response.status === 200) {
+		return response.data as createCalendarResponseType;
+	} else return undefined;
 };
