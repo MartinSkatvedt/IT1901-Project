347c9e631d0488d73fb9b2acb39ce0f45dfff158
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2021-11-28 20:29:55 +0100

    added logout button (#77)

--- calendar/fxui/src/main/java/calendar/ui/CalendarApp.java
+++ calendar/fxui/src/main/java/calendar/ui/CalendarApp.java
@@ -9,22 +9,25 @@ import javafx.stage.Stage;
 
 public class CalendarApp extends Application {
     private Stage stage;
-    
+
     @Override
     public void start(Stage firstStage) throws Exception {
         this.stage = firstStage;
         firstStage.setResizable(false);
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        LoginController controller = new LoginController();
+        loader.setController(controller);
+        Parent root = loader.load();
         firstStage.setTitle("CoolCalendar");
         firstStage.setScene(new Scene(root, 350, 400));
         firstStage.show();
     }
-    
+
     public void changeScene(String fxml) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource(fxml));
         Scene scene = new Scene(parent, 600, 500);
         this.stage.setScene(scene);
-    } 
+    }
 
     public static void main(String[] args) {
         launch(CalendarApp.class, args);
